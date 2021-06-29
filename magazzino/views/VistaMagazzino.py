07b0caa14de2d5e5605d5194c16222396e899210
Marco Xu <s1092678@studenti.univpm.it>
2021-06-29 16:21:03 +0200

    wah

--- magazzino/views/VistaMagazzino.py
+++ magazzino/views/VistaMagazzino.py
@@ -1,22 +1,24 @@
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QListView, QVBoxLayout, QPushButton, QMessageBox, QGridLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QListView, QVBoxLayout, QPushButton, QMessageBox
 
-from magazzino.controller.ControlloreMagazzino import ControlloreMagazzino
+from listaordinazione.controller.ControlloreListaOrdinazione import ControlloreListaOrdinazione
+from ordinazione.views.VistaOrdinazione import VistaOrdinazione
 
-class VistaMagazzino(QWidget):
+
+class VistaListaOrdinazione(QWidget):
 
     def __init__(self):
-        super(VistaMagazzino, self).__init__()
+        super(VistaListaOrdinazione, self).__init__()
 
-        self.controller = ControlloreMagazzino()
+        self.controller = ControlloreListaOrdinazione()
 
         self.h_layout = QHBoxLayout()
         self.list_view = QListView()
         self.listview_model = QStandardItemModel(self.list_view)
 
-        for ingrediente, qt in self.controller.get_magazzino().items():
+        for ordinazione in self.controller.get_lista_ordinazione():
             item = QStandardItem()
-            item.setText("ingrediente:{} ".format(ingrediente) + "          quantità:{} ".format(qt))
+            item.setText("nome:{} ".format(ordinazione.get_nome()) + "          tavolo:{} ".format(ordinazione.get_tavolo()))
             item.setEditable(False)
             font = item.font()
             font.setPointSize(18)
@@ -26,42 +28,26 @@ class VistaMagazzino(QWidget):
         self.h_layout.addWidget(self.list_view)
 
         buttons_layout = QVBoxLayout()
-        add_layout = QGridLayout()
 
-        self.label_ingrediente = QLabel('<font size="4"> Ingrediente </font>')
-        self.ingrediente = QLineEdit()
-        add_layout.addWidget(label_ingrediente, 0, 0)
-        add_layout.addWidget(self.ingrediente, 0, 1)
+        open_button = QPushButton("Apri")
+        open_button.clicked.connect(self.show_selected_info)
+        buttons_layout.addWidget(open_button)
 
-        self.label_quantita = QLabel('<font size="4"> Quantità </font>')
-        self.quantita = QLineEdit()
-        add_layout.addWidget(label_quantita, 1, 0)
-        add_layout.addWidget(self.quantita, 1, 1)
-
-        buttons_layout.addLayout(add_layout)
-
-        add_button = QPushButton("Aggiungi ingrediente")
-        add_button.clicked.connect(self.show_selected_info)
-        buttons_layout.addWidget(add_button)
-
-        edit_button = QPushButton("Modifica ingrediente")
-        edit_button.clicked.connect(self.edit_button)
-        buttons_layout.addWidget(edit_button)
+        delete_button = QPushButton("Elimina ordine")
+        delete_button.clicked.connect(self.delete_ordinazione)
+        buttons_layout.addWidget(delete_button)
 
         buttons_layout.addStretch()
         self.h_layout.addLayout(buttons_layout)
 
         self.setLayout(self.h_layout)
         self.resize(600, 300)
-        self.setWindowTitle("Magazzino")
+        self.setWindowTitle("Lista Ordinazione")
 
     def closeEvent(self, event):
+        print("ON CLOSE")
         self.controller.save_data()
         event.accept()
-
-    def add_ingrediente(self):
-        self.vista_inserisci_ingrediente = VistaInserisciIngrediente(self.controller, self.update_ui)
-        self.vista_inserisci_ingrediente.show()
 
     def show_selected_info(self):
         if len(self.list_view.selectedIndexes()) > 0:
